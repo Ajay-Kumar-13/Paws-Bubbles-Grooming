f7cbdd4086a1993c6990bf9c130a9dc5f34dd564
Ajay-Kumar-13 <apulleti@gitam.in>
2024-06-19 14:30:08 +0000

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+
+function Navbar() {
+    const [online, setOnline] = useState(false);
+    return (
+        <React.Fragment>
+            <div className="navbar">
+                <div className="d-flex align-items-center">
+                    <label class="switch">
+                        <input type="checkbox" onClick={() => { setOnline(!online) }} />
+                        <span class="slider round"></span>
+                    </label>
+                    <div className={`status ${online && 'active'}`}></div>
+                </div>
+                <div className="user d-flex align-items-center justify-content-center">
+                    A
+                </div>
+            </div>
+            <hr></hr>
+            <div className="d-flex justify-content-center p-2 title">
+                Paws and Bubbles
+            </div>
+            <div className="content">
+                <div className="row">
+                    <div className="col-6 raleWay d-flex justify-content-center align-items-center">
+                        <div className="dashboardIcon">
+                            <i class="fa-solid fa-coins fa-2x"></i>
+                        </div>
+                        <div className="iconText">
+                            <p>Earnings</p>
+                            <span className="caudex">500 $</span>
+                        </div>
+                    </div>
+
+                    <div className="col-6 raleWay d-flex justify-content-center align-items-center">
+                        <div className="dashboardIcon">
+                            <i class="fa-solid fa-person-biking fa-2x"></i>
+                        </div>
+                        <div className="iconText">
+                            <p>Trips</p>
+                            <span className="caudex">500 $</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="row mt-4">
+                    <div className="col-6 raleWay d-flex justify-content-center align-items-center">
+                        <div className="dashboardIcon">
+                            <i class="fa-solid fa-star fa-2x"></i>
+                        </div>
+                        <div className="iconText">
+                            <p>Ratings</p>
+                            <span className="caudex">500 $</span>
+                        </div>
+                    </div>
+                    <div className="col-6 raleWay d-flex justify-content-center align-items-center">
+                        <div className="dashboardIcon">
+                            <i class="fa-solid fa-map-location-dot fa-2x"></i>
+                        </div>
+                        <div className="iconText">
+                            <p>KM</p>
+                            <span className="caudex">500 $</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </React.Fragment>
+    )
+}
+
+export default Navbar;
---
+++ src/components/dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+function Dashboard() {
+    return (
+        <React.Fragment>
+            <Navbar />
+        </React.Fragment>
+    )
+}
+
+export default Dashboard;
